57eab8490eb91f98217eab50d63dd6ad3688052b
Annisaseptiyani <anisaseptiani475@gmail.com>
2025-06-05 16:01:42 +0700

    Login

---
+++ app/src/main/java/id/ac/unpas/r_bmanajementugas/login/HomeScreen.kt
@@ -0,0 +1,9 @@
+package id.ac.unpas.r_bmanajementugas.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Welcome to Home Screen!")
+}
